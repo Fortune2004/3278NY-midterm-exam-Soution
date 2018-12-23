277fce77f5e531b601da38ac409b075c06d331ab
salmaalam <salmaalam12122004@gmail.com>
2018-12-23 15:26:26 -0500

    6th commit

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -10,15 +10,16 @@ public class Fibonacci {
         int fibo[] = new int[40];
         for (int i = 0; i<40; i++){
             if (i < 2) {
-                fibo[i] = 1;
+                fibo[i] = i;
             }else{
-                fibo[i]=fibo[i=2] + fibo [i-1] ;
+                fibo[i]=fibo[i-2] + fibo [i-1] ;
             }
         }
         System.out.println(Arrays.toString(fibo));
     }
 
 }
+
 
 
 
