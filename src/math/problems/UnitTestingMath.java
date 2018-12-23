c47ebaddefb597228f2e290338d5adff0a9610af
salmaalam <salmaalam12122004@gmail.com>
2018-12-23 15:04:32 -0500

    5th commit

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -2,7 +2,6 @@ package math.problems;
 
 public class UnitTestingMath {
     public static void main(String[] args) {
-        //Apply Unit testing into each classes and methods in this package.
-
     }
 }
+        //Apply Unit testing into each classes and methods in this package.
