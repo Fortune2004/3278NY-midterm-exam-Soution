cad5643f2965a2a90888afb8df253b78bc7e5844
salmaalam <salmaalam12122004@gmail.com>
2018-12-31 02:15:21 -0500

    Correction

---
+++ src/design/Origin.java
@@ -0,0 +1,11 @@
+package design;
+
+public abstract class Location {
+
+
+        final String p1 = "Dhaka";
+        final String p2 = "Comilla";
+        final String p3 = "Chittagong";
+
+        public abstract void travaletime();
+    }
